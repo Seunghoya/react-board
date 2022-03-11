784fb9fe123c35566b78ae08c5ab8851bf46a9b7
Seunghoya <viva.primavera121@gmail.com>
2022-03-11 15:25:12 +0900

    Add 삭제기능

--- src/pages/ViewArticle/ViewArticle.js
+++ src/pages/ViewArticle/ViewArticle.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { useLocation, useHistory } from 'react-router-dom';
 import { Button } from '../../components/Button/Button';
 import { ViewContainer, 
   ViewHeaderContainer, 
@@ -9,20 +9,34 @@ import { ViewContainer,
   Date,
   ContentContainer, 
   ButtonContainer } from './ViewArticleStyle';
+import axios from 'axios';
 import { dummyContent } from '../../dummy/dummyContent';
 export const ViewArticle = () => {
 
-
+  const history = useHistory()
   const location = useLocation();
   const ArticleDate = location.state.ArticleDate;
 
   console.log(ArticleDate)
+
+  const goback = () => {
+    history.push('/')
+  }
   const reqEdit = () => {
     console.log('수정버튼 클릭')
   }
 
   const reqDelete = () => {
-    console.log('삭제버튼 클릭')
+    axios.delete(`http://localhost:4000/article/${ArticleDate.id}`)
+      .then((data) => {
+        console.log(data)
+        history.push('/');
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+
+    
   }
 
   return (
@@ -37,6 +51,7 @@ export const ViewArticle = () => {
         <div>{ArticleDate.content}</div>
       </ContentContainer>
       <ButtonContainer>
+        <Button style={{'margin':'10px'}} onClick={goback}>목록으로</Button>
         <Button style={{'margin':'10px'}} onClick={reqEdit}>수정</Button>
         <Button style={{'margin':'10px'}} onClick={reqDelete}>삭제</Button>
       </ButtonContainer>
